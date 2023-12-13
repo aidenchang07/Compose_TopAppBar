8b3a53a9511659c3ddb98b808d29b40109abf311
Aiden Chang <asdf62210@gmail.com>
2023-12-13 20:12:14 +0800

    Add MainScreen

--- app/src/main/java/com/example/compose_topappbar/MainActivity.kt
+++ app/src/main/java/com/example/compose_topappbar/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.compose_topappbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_topappbar.ui.screens.MainScreen
 import com.example.compose_topappbar.ui.theme.Compose_TopAppBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_TopAppBarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_TopAppBarTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/compose_topappbar/ui/screens/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.example.compose_topappbar.ui.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by AidenChang 2023/12/13
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
